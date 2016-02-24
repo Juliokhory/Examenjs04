c9634aefe00a217439594e190c002cac8a89fdf1
Julio Ortiz <jcortizojeda@gmail.com>
2016-02-24 15:32:59 -0500

    segunda presentacion

--- examen.js
+++ examen.js
@@ -2,23 +2,45 @@ console.log("dsd");
 
 var votantes=[];
 
+// agregarV(1,"luz","SI","cocha")
+// agregarV(2,"julio","NO","lema")
+
 function agregarV(n,un,sn,c){
 
     var na = [];
-    for(var i =0; i<arguments; i++){
+    for(var i =0; i<arguments.length; i++){
         console.log(arguments[i]);
         na.push(arguments[i]);
     }
-    console.log(na);
+
     votantes.push(na);
     console.log(votantes);
 }
-//invirtiendo
-Array.prototype.invertirUsername = function(f){
-    var w = this;
-    return f(w);
+//Invertir Posicion
+Array.prototype.invertirPosicion = function(){
+    return this.reverse();
 }
 
-function invertir(e){
-    return e.reverse();
+//Cambiar SI o NO
+Array.prototype.cambiarSN = function(e){
+    var el = this[2];
+    if(el=="SI"){
+        this[2]="NO";
+    }else if(el=="NO"){
+        this[2]="SI";
+    }else{
+        console.log("es otra elecion");
+    }
+    return this;
 }
+
+
+//invirtiendo
+// Array.prototype.invertirPosicion = function(f){
+//     var w = this;
+//     return f(w);
+// }
+//
+// function invertir(e){
+//     return e.reverse();
+// }
